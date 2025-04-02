888d8e50a18a87e5c52cd2cea0dab89db33078ed
huydv398 <huydv398@gmail.com>
2025-04-02 08:39:45 +0700

    update playbook semaphore

--- semaphore/3.script.sh
+++ semaphore/3.script.sh
@@ -3,7 +3,8 @@
 backup_day=$(date +'%Y%m%d')
 clean_day=$(date --date="3 day ago" +'%Y%m%d')
 
-
+apiToken='7895711577:AAG1IX5clX5hmQ_Z3jvDlpYgP3cSxj2kc_k'
+chatId='750878537'
 #echo $message
 
 message="""\`[Backup Volume CEPH Daily
